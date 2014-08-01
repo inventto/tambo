5c0e028bc42e991cd32069a4f623a048df4baff2
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-08-01 10:36:52 -0300

    Limitado o tamanho das imagens e corrigido os specs.

--- spec/models/pessoa_spec.rb
+++ spec/models/pessoa_spec.rb
@@ -2,28 +2,25 @@ require 'rails_helper'
 require "paperclip/matchers"
 
 RSpec.describe Pessoa, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   it "pessoa deve ter nome" do
-    should validate_presenca_of :nome
+    should validate_presence_of :nome
   end
   it "pessoa deve ter biografia" do
-    should validate_presenca_of :biografia
+    should validate_presence_of :biografia
   end
   it "pessoa deve ter url" do
-    should validate_presenca_of :url
+    should validate_presence_of :url_unica
   end
   it "pessoa deve ter email" do
-    should validate_presenca_of :email
+    should validate_presence_of :email
   end
   it "pessoa deve estar realcionada com cidade" do
-    should validate_belong_to :cidade
-    slould validate_associated :cidade
+    should belong_to :cidade
+    #should validate_presence_of :cidade
   end
   it "pessoa deve ter imagem" do
     have_attached_file(:foto_perfil)
     have_attached_file(:foto_capa)
-    should validate_attachment_presence(:foto_perfil)
-    should validate_attachment_presence(:foto_capa)
     should validate_attachment_content_type(:foto_perfil). allowing('image/png', 'image/gif'). rejecting('text/plain', 'text/xml')
     should validate_attachment_content_type(:foto_capa). allowing('image/png', 'image/gif'). rejecting('text/plain', 'text/xml')
     should validate_attachment_size(:foto_perfil). less_than(2.megabytes)
