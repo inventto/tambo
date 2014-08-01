586787257ad60ebd4f665aa59c1efcc400b7b36e
Wellington Mitrut <wellingtonmitrut@gmail.com>
2014-08-01 10:27:08 -0300

    Testes Vitrine OK

--- spec/models/vitrine_spec.rb
+++ spec/models/vitrine_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Vitrine, :type => :model do
-   #it { should has_and_belongs_to_many(:categoria) }
+  it { should have_and_belong_to_many(:categorias) }
   it { should validate_presence_of(:nome) }
   it { should validate_uniqueness_of(:nome) }
   it { should have_attached_file(:imagem)}
