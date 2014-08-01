50d94b4afbb5306bed3e409784c884e4b211c9c3
Jônatas Paganini <jonatasdp@gmail.com>
2014-08-01 09:48:22 -0300

    starting cidade spec

--- app/models/cidade.rb
+++ app/models/cidade.rb
@@ -1,6 +1,6 @@
 class Cidade < ActiveRecord::Base
   has_many :pessoas
-  validates :nome, presence: true
+  validates :nome, presence: true, uniqueness: true
   validates :estado, inclusion: {in: %w(SC PR SP MG)}, presence: true
   def to_s
     [nome, estado].join(" - ")
--- spec/models/cidade_spec.rb
+++ spec/models/cidade_spec.rb
@@ -1,5 +1,11 @@
+# encoding: utf-8
 require 'rails_helper'
 
 RSpec.describe Cidade, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:nome) }
+  it { should validate_presence_of(:nome) }
+  it { should validate_uniqueness_of(:nome) }
+  it "não pode criar uma cidade em um estado invalido" do
+    expect(cidade.errors).to have_key(:estado)
+  end
 end
