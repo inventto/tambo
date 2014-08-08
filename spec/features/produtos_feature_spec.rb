75f2f7b4bd78451177f81795399490863cf392b9
Jose Luiz Tafarel <jltafarel@gmail.com>
2014-08-08 15:55:35 -0300

    testes de navegação para as views do produto

--- spec/features/produtos_feature_spec.rb
+++ spec/features/produtos_feature_spec.rb
@@ -1,4 +1,20 @@
 require "rails_helper"
 
 describe "produtos", :type => :feature do
+  context "listar" do
+    before :all do
+      @produto = Produto.first || FactoryGirl.create(:produto)
+    end
+    it "espera ter produtos" do
+      visit "/produtos"
+      expect(page).to have_content "Produtos"
+      expect(page).to have_content @produto.nome
+      click_link(@produto.nome)
+      expect(page.current_path).to eql("/#{@produto.url_unica}")
+      expect(page).to have_content @produto.nome
+    end
+    after :all do
+      Produto.destroy_all
+    end
+  end
 end
