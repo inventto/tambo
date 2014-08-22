eb758a80d091b85fe5edeea6fc00a8e4a42bf049
Jônatas Paganini <jonatasdp@gmail.com>
2014-08-22 11:09:11 -0300

    host name for paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:url] ='http://emporiotambo.herokuapp.com' 
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'sa-east-1.amazonaws.com'
