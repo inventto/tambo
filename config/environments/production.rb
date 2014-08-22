a5b7065afa3b4e8f6aed4fb9c93b73abcdafbeac
Jônatas Paganini <jonatasdp@gmail.com>
2014-08-22 11:13:24 -0300

    removing s3 region and removing protocol on domain name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.paperclip_defaults = {
     storage: :s3,
-    s3_host_name: 'sa-east-1.amazonaws.com',
     s3_credentials: {
         bucket: ENV['S3_BUCKET_NAME'],
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
       }
   }
-  puts "paperclip conf: #{ config.paperclip_defaults.inspect } "
 end
 GA.tracker = "UA-38318580-4"
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,2 @@
-Paperclip::Attachment.default_options[:url] ='http://emporiotambo.herokuapp.com' 
+Paperclip::Attachment.default_options[:url] ='emporiotambo.herokuapp.com' 
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'sa-east-1.amazonaws.com'
