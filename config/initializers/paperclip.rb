9f964d42ca41fbba0d1510241d1e0430cb34c669
Jônatas Paganini <jonatasdp@gmail.com>
2014-08-22 10:13:48 -0300

    fixing host url

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:url] ='emporiotambo.herokuapp.com' 
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'ec2.sa-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'sa-east-1.amazonaws.com'
