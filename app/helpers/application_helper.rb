de0dff7e774cf6aa2d13b3a4cd8ef769c1f1df06
Jônatas Paganini <jonatasdp@gmail.com>
2014-08-22 16:40:04 -0300

    fixing details on layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,13 @@ module ApplicationHelper
       super(name, opts)
     end
   end
-  def show_first_image_or_default_for(produto)
+  def show_first_image_or_default_for(produto, opts={})
     src =
       if not produto.imagens.blank?
         produto.imagens.first.anexo.url(:thumb)
       else
         'imagem_produto_default.png'
       end
-    image_tag(src)
+    image_tag(src, opts)
   end
 end
